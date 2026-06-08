c3e942711c36d39c83719860b7b7211a44c40bfa
Wendutechxpert <wendutechxpert@gmail.com>
2026-06-08 13:17:54 +0100

    Task: Reformated the folder and files structure, and also install parcel and using it for dynamic referencing, like calling style sheets, scripts, image ETC

---
+++ admin/config.js
@@ -0,0 +1,3 @@
+window.host = "the-revit-systems-site-1.onrender.com";
+window.baseURL = `http://localhost:3000`;
+// https://${window.host}
--- assets/js/config.js
+++ assets/js/config.js
@@ -1,3 +1,2 @@
 window.host = "the-revit-systems-site-1.onrender.com";
-window.baseURL = `http://localhost:3000`;
-// https://${window.host}
+window.baseURL = `https://${window.host}`;
